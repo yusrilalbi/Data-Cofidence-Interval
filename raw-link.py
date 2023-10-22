64a027ba22c73dc0db429e7258abd65a21d78390
yusrilalbi <60544526+yusrilalbi@users.noreply.github.com>
2023-10-22 03:07:48 +0000

    feat: rename

---
+++ raw-link.py
@@ -0,0 +1,19 @@
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM-RNN/LSTM_RNN_FORECAST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM-RNN/LSTM_RNN_LOST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM-RNN/LSTM_RNN_MAE.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM-RNN/LSTM_RNN_PREDICT.xlsx
+
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM/LSTM_FORECAST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM/LSTM_LOST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM/LSTM_MAE.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/LSTM/LSTM_PREDICT.xlsx
+
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN-LSTM/RNN_LSTM_FORECAST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN-LSTM/RNN_LSTM_LOST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN-LSTM/RNN_LSTM_MAE.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN-LSTM/RNN_LSTM_PREDICT.xlsx
+
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN/RNN_FORECAST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN/RNN_LOST.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN/RNN_MAE.xlsx
+!wget https://github.com/yusrilalbi/Data-Cofidence-Interval/raw/main/RNN/RNN_PREDICT.xlsx
